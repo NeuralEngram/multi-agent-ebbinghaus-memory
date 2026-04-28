d2a75fa9dbee12e51919546a0250040fd1e8fc4c
darurpoojitha <dpoojitha263@gmail.com>
2026-04-28 20:12:47 +0530

    fixing model text

--- memory_core/episodic_memory.py
+++ memory_core/episodic_memory.py
@@ -65,10 +65,10 @@ W_TASK_SUCCESS     = 0.30
 
 def _get_embedding(text: str):
     result = genai.embed_content(
-        model="models/text-embedding-004",
-        content=text,
-        task_type="retrieval_document"
-    )
+    model="models/text-embedding-004",
+    content=text,
+)
+   
     return result["embedding"]
 
 
